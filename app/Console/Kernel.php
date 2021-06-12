d9c33d3f3ab8269aeffad478e48f66bdc525b633
zhuchunshu <laravel@88.com>
2021-06-12 19:28:04 +0800

    更新: 接入Laravel Telescope

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('telescope:prune')->daily();
         // $schedule->command('inspire')->hourly();
     }
 
