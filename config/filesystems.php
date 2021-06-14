535fa2e96d69c030a58ecacac0675ff5854a3f9d
zhuchunshu <laravel@88.com>
2021-06-14 22:59:05 +0800

    更新: 精简配置

--- config/filesystems.php
+++ config/filesystems.php
@@ -45,17 +45,7 @@ return [
             'root' => public_path('upload'),
             'url' => '/upload',
             'visibility' => 'public',
-        ],
-
-        's3' => [
-            'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-        ],
+        ]
 
     ],
 
